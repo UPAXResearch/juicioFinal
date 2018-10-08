3668cab0501205c013f4e182ed380d1d3f07ac61
Jose D <jadm333@gmail.com>
2018-10-08 16:55:21 -0500

    Add extra to poneTruesBase

--- R/poneTruesBase.R
+++ R/poneTruesBase.R
@@ -1,15 +1,18 @@
-#'Titulo Pendiente
+#' Convertir columna en logica
 #'
-#'Descripción pendiente
-#'@param x Pendiente
-#'@export
-#'@keywords frecuencias
-#'@examples
-#'pendiente
+#' @param base La base a considerar
+#' @param variables Las variables a convertir, meterla como vector de strings
+#' @param extra Algunos caracteres/texto extra para poner FALSE en estos casos
+#'
+#' @return Data.frame
+#' @export
+#'
+#' @examples
 
-poneTruesBase<-function(base,variables){
+poneTruesBase<-function(base,variables, extra = c()){
+
   for(i in 1:length(variables)){
-    base[,variables[i]]<-poneTruesVar(base[,variables[i]])
+    base[,variables[i]]<-poneTruesVar(base[,variables[i]], extra)
     print(str(base[,variables[i]]))
   }
   return(base)
--- R/poneTruesVar.R
+++ R/poneTruesVar.R
@@ -1,13 +1,14 @@
-#'Titulo Pendiente
+#' Función interta que usa poneTruesBase
 #'
-#'Descripción pendiente
-#'@param x Pendiente
-#'@export
-#'@keywords frecuencias
-#'@examples
-#'pendiente
-
-poneTruesVar<-function(x){
+#' @param x
+#' @param extra
+#'
+#' @return
+#' @export
+#'
+#' @examples
+poneTruesVar<-function(x, extra){
+  extra = c(c('',' ','0',NA), extra)
   x<-as.character(ifelse(x%in%c('',' ','0',NA),NA,TRUE))
   # x[x==''|x=='0']<-NA
   # x[x!=''|x!='0']<-TRUE
