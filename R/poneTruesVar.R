d9ea29c60cc8240aba4fb1667f85391605db0f84
Jose D <jadm333@gmail.com>
2018-10-08 17:32:21 -0500

    minor fix

--- R/poneTruesVar.R
+++ R/poneTruesVar.R
@@ -9,9 +9,9 @@
 #' @examples
 poneTruesVar<-function(x, extra){
   extra = c(c('',' ','0',NA), extra)
-  x<-as.character(ifelse(x%in%c('',' ','0',NA),NA,TRUE))
+  x <- as.character(ifelse(x %in% extra,NA,TRUE))
   # x[x==''|x=='0']<-NA
   # x[x!=''|x!='0']<-TRUE
-  x<-as.logical(x)
+  x < -as.logical(x)
   return(x)
 }
